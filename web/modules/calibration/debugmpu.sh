179e8b17b4379dfa97ce7e78bf1f7fd5a0e97087
avrouwenve <avrouwenve@merative.com>
2023-04-08 15:04:00 -0400

    Changes based on empirical data

--- web/modules/calibration/debugmpu.sh
+++ web/modules/calibration/debugmpu.sh
@@ -4,6 +4,6 @@ then
    exit 1
 fi
 sudo modprobe i2c-dev
-cd ..
+cd /mnt/mmcblk0p2/apps/adrianAutoPilot
 export PYTHONPATH=/mnt/mmcblk0p2/apps/adrianAutoPilot/web
 python web/modules/mpu9250Interface.py
